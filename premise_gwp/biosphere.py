9076d5b6c16780c0bf6255aa825b991fd6616d18
romainsacchi <r_s@me.com>
2021-10-04 19:11:24 +0200

    Update biosphere.py

--- premise_gwp/biosphere.py
+++ premise_gwp/biosphere.py
@@ -1,6 +1,5 @@
 import bw2data as bd
 
-
 def check_biosphere_database():
     ERROR = "IPCC LCIA methods for ecoinvent biosphere flows only; install base ecoinvent data"
     assert "biosphere3" in bd.databases, ERROR
