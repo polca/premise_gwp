f72042025075ab54f901889c58003e2967bf42b4
romainsacchi <r_s@me.com>
2024-08-22 13:07:37 +0200

    ei310 compatibility

--- premise_gwp/__init__.py
+++ premise_gwp/__init__.py
@@ -7,16 +7,16 @@ from pathlib import Path
 
 DATA_DIR = Path(__file__).resolve().parent / "data"
 
-import bw2io
+import bw2io, bw2data
 from bw2io import ExcelLCIAImporter
 
-from .biosphere import check_biosphere_database, check_biosphere_version
+from .biosphere import check_biosphere_database, check_biosphere_version, load_ei310_mapping
 from .version import version as __version__
 
 
 def add_premise_gwp():
-    check_biosphere_database()
-    bw2io_version = check_biosphere_version()
+    biosphere_name = check_biosphere_database()
+    bw2io_version = check_biosphere_version(biosphere_name)
 
     # impact methods to create
     categories_bw2io087 = {
@@ -83,19 +83,44 @@ def add_premise_gwp():
             filepath=DATA_DIR / c[-1], name=c[0], unit=c[1], description=c[2]
         )
 
-        # apply formatting strategies
-        category.apply_strategies()
-
         # if bw2io < 0.8.6
         if bw2io.__version__ < (0, 8, 6):
             if len(list(category.unlinked)) == 1:
                 if list(category.unlinked)[0]["name"] == "Carbon dioxide, in air":
                     category.drop_unlinked()
 
+        if bw2io_version == (0, 8, 12):
+            print("Converting to ei 3.10 biosphere names")
+            mapping = load_ei310_mapping()
+            for method in category.data:
+                for flow in method["exchanges"]:
+                    flow["name"] = mapping.get(flow["name"], flow["name"])
+
+
+
+        # apply formatting strategies
+        category.apply_strategies()
+
+
         # check that no flow is unlinked
-        assert len(list(category.unlinked)) == 0, "Unlinked flows: {}".format(
-            list(category.unlinked)
-        )
+        if len(list(category.unlinked)) > 0:
+            if bw2io_version == (0, 8, 12):
+                if all(
+                    flow["categories"] == ('air', 'lower stratosphere + upper troposphere')
+                    for flow in category.unlinked
+                ):
+                    category.drop_unlinked()
+
+            print(f"{len(list(category.unlinked))} unlinked flows:")
+            for flow in category.unlinked:
+                print(flow)
+
+            if len(list(category.unlinked)) > 0:
+                raise ValueError("Unlinked flows")
 
         # write method
-        category.write_methods(overwrite=True, verbose=True)
+        category.name = c[0]
+        category.write_methods(overwrite=True, verbose=True,)
+        method = [m for m in bw2data.methods if m == c[0]][0]
+        m = bw2data.Method(method)
+        m.metadata["name"] = c[0]
--- premise_gwp/biosphere.py
+++ premise_gwp/biosphere.py
@@ -1,16 +1,46 @@
 import bw2data as bd
+from pathlib import Path
+import yaml
+
+DATA_DIR = Path(__file__).resolve().parent / "data"
+
+
+def load_ei310_mapping():
+    with open(DATA_DIR / "mapping_ei310.yaml", "r", encoding="utf-8") as stream:
+        mappings = yaml.safe_load(stream)
+    return mappings
+
+def check_presence_biosphere_database(biosphere_name: str) -> str:
+    """
+    Check that the biosphere database is present in the current project.
+    """
+
+    if biosphere_name not in bd.databases:
+        print("RELICS requires the name of your biosphere database.")
+        print(
+            "Please enter the name of your biosphere database as it appears in your project."
+        )
+        print(bd.databases)
+        biosphere_name = input("Name of the biosphere database: ")
+
+    return biosphere_name
 
 
 def check_biosphere_database():
+    biosphere_name = check_presence_biosphere_database("biosphere3")
     ERROR = "IPCC LCIA methods for ecoinvent biosphere flows only; install base ecoinvent data"
-    assert "biosphere3" in bd.databases, ERROR
+    assert biosphere_name in bd.databases, ERROR
+    return biosphere_name
 
 
-def check_biosphere_version() -> tuple:
+def check_biosphere_version(biosphere_name) -> tuple:
     # check for the presence of Beryllium II
-    if "Beryllium II" not in [f["name"] for f in bd.Database("biosphere3")]:
+    if "Beryllium II" not in [f["name"] for f in bd.Database(biosphere_name)]:
         bw2io_version = (0, 8, 7)
     else:
-        bw2io_version = (0, 8, 8)
+        if "Methylchloride" in [f["name"] for f in bd.Database(biosphere_name)]:
+            bw2io_version = (0, 8, 12)
+        else:
+            bw2io_version = (0, 8, 8)
 
     return bw2io_version
