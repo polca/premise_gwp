1230ded26789c4dc8003961b21afe968aa1efae7
herlaarsdm <sjoerd.herlaar@tno.nl>
2025-07-23 08:29:33 +0200

    changed how bw2io versioning is checked in line 92

--- premise_gwp/__init__.py
+++ premise_gwp/__init__.py
@@ -89,7 +89,7 @@ def add_premise_gwp():
         )
 
         # if bw2io < 0.8.6
-        if bw2io.__version__ < (0, 8, 6):
+        if tuple(map(int, bw2io.__version__.split('.'))) < (0, 8, 6):
             if len(list(category.unlinked)) == 1:
                 if list(category.unlinked)[0]["name"] == "Carbon dioxide, in air":
                     category.drop_unlinked()
